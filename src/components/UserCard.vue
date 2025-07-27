77430862c5fa613b85d45ff9eb4da2029f447594
Kalaiselvi Gunasekar <kalaisudha01@gmail.com>
2025-07-27 19:16:33 +0530

    second test

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -8,7 +8,7 @@
 
     <h1 v-if="awesome">Vue is awesome!
     </h1>
-    <h1 v-else>Oh  No changes nade 😢</h1>
+    <h1 v-else>Oh  kalaitest2</h1>
     <ul v-if="isActive">
       <li v-for="item in items" :key="item">{{ item }}</li>
     </ul>
